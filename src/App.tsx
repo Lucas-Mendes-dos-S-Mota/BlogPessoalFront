6174140fcd2a39c22d146512a9af91fb700b4100
Lucas Mendes dos Santos Mota <lucas.mendes0515@gmail.com>
2022-09-30 16:09:06 -0300

    React 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import {Grid} from "@material-ui/core"
+import {Grid, } from "@material-ui/core"
 import Home from "./paginas/home/Home";
 import './App.css';
+import Login from './paginas/login/Login';
+
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-      <Footer />
-    </>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+    <Footer />
+  </Router>
+
   );
 }
 
--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,35 +4,36 @@ import { Box } from "@mui/material";
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import'./Footer.css';
 
 function Footer(){
     return(
         <>
              <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="texto">Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/Lucas-Mendes-dos-S-Mota" target="_blank">
-                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                                <GitHubIcon className="rede" />
                             </a>
                             <a href="https://instagram.com/lucas_mendes_l_?igshid=YmMyMTA2M2Y=" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className="rede" />
                             </a>
                             <a href="https://www.linkedin.com/in/lucas-mendes-32135a23a/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className="rede" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="box2">
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="texto" >© 2022 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom className="texto" align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
